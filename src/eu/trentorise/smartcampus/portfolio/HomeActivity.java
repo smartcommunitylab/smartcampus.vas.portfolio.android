2f4d8324b9279005955a148b0f4e0745b15a3012
Giovanni De Francesco <posta.giovanni.defrancesco@gmail.com>
2014-02-19 11:04:06 +0100

    uses launcherHelper class from common

--- src/eu/trentorise/smartcampus/portfolio/HomeActivity.java
+++ src/eu/trentorise/smartcampus/portfolio/HomeActivity.java
@@ -36,6 +36,7 @@ import com.actionbarsherlock.view.MenuItem;
 
 import eu.trentorise.smartcampus.ac.AACException;
 import eu.trentorise.smartcampus.ac.SCAccessProvider;
+import eu.trentorise.smartcampus.android.common.LauncherHelper;
 import eu.trentorise.smartcampus.android.common.SCAsyncTask;
 import eu.trentorise.smartcampus.portfolio.frags.PortfolioFragment;
 import eu.trentorise.smartcampus.portfolio.frags.PortfoliosListFragment;
@@ -57,7 +58,8 @@ import eu.trentorise.smartcampus.protocolcarrier.exceptions.SecurityException;
  * @author Simone Casagranda
  * 
  */
-public class HomeActivity extends SherlockFragmentActivity implements FragmentLoader, NoteLayerInteractor, SharedPortfolio {
+public class HomeActivity extends SherlockFragmentActivity implements
+		FragmentLoader, NoteLayerInteractor, SharedPortfolio {
 
 	private Button mSlidingButton;
 	private EditText mNotesEditText;
@@ -98,11 +100,14 @@ public class HomeActivity extends SherlockFragmentActivity implements FragmentLo
 		if (mSlidingDrawer.isShown() && mSlidingDrawer.isOpened()) {
 			mSlidingDrawer.animateClose();
 		} else {
-			Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+			Fragment currentFragment = getSupportFragmentManager()
+					.findFragmentById(R.id.fragment_container);
 			// Checking if there is a fragment that it's listening for back
 			// button
-			if (currentFragment != null && currentFragment instanceof OnBackPressedListener) {
-				managed = ((OnBackPressedListener) currentFragment).onBackPressed();
+			if (currentFragment != null
+					&& currentFragment instanceof OnBackPressedListener) {
+				managed = ((OnBackPressedListener) currentFragment)
+						.onBackPressed();
 			}
 			// If it's not managed we can continue
 			if (!managed) {
@@ -124,13 +129,20 @@ public class HomeActivity extends SherlockFragmentActivity implements FragmentLo
 		try {
 			// Loading first fragment that works as home for application.
 			// Getting token
-			if (!isViewer() && (savedInstanceState == null || !savedInstanceState.getBoolean("initialized"))) {
-				FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+			if (!isViewer()
+					&& (savedInstanceState == null || !savedInstanceState
+							.getBoolean("initialized"))) {
+				FragmentTransaction ft = getSupportFragmentManager()
+						.beginTransaction();
 				Fragment frag = new PortfoliosListFragment();
-				ft.replace(R.id.fragment_container, frag).commitAllowingStateLoss();
-			} else if (savedInstanceState != null && savedInstanceState.containsKey("sharedPortfolio")) {
-				sharedPortfolioContainer = savedInstanceState.getParcelable("sharedPortfolio");
-				if (!sharedPortfolioContainer.getPortfolio().entityId.equals(mPortfolioEntityId)) {
+				ft.replace(R.id.fragment_container, frag)
+						.commitAllowingStateLoss();
+			} else if (savedInstanceState != null
+					&& savedInstanceState.containsKey("sharedPortfolio")) {
+				sharedPortfolioContainer = savedInstanceState
+						.getParcelable("sharedPortfolio");
+				if (!sharedPortfolioContainer.getPortfolio().entityId
+						.equals(mPortfolioEntityId)) {
 					new LoadPortfolioAsyncTask().execute(mPortfolioEntityId);
 				}
 			} else {
@@ -148,22 +160,31 @@ public class HomeActivity extends SherlockFragmentActivity implements FragmentLo
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// Checking start action
-		if (isViewer()) {
-			mPortfolioEntityId = getIntent().getStringExtra(getString(R.string.view_intent_arg_object_id));
-		}
-
-		initDataManagement(savedInstanceState);
-
-		try {
-			if (!PMHelper.getAccessProvider().login(this, null)) {
-				initData(savedInstanceState);
+		if (LauncherHelper.isLauncherInstalled(this, true)) {
+			// Checking start action
+			if (isViewer()) {
+				mPortfolioEntityId = getIntent().getStringExtra(
+						getString(R.string.view_intent_arg_object_id));
 			}
-		} catch (AACException e) {
-			PMHelper.endAppFailure(this, R.string.app_failure_setup);
-			return;
-		}
 
+			initDataManagement(savedInstanceState);
+
+			try {
+				if (!PMHelper.getAccessProvider().login(this, null)) {
+					initData(savedInstanceState);
+				}
+			} catch (AACException e) {
+				PMHelper.endAppFailure(this, R.string.app_failure_setup);
+				return;
+			}
+
+			setupContent();
+
+			initialized = true;
+		}
+	}
+
+	private void setupContent() {
 		// Asking for windows features
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		// Setting content view
@@ -178,21 +199,25 @@ public class HomeActivity extends SherlockFragmentActivity implements FragmentLo
 			public void onDrawerOpened() {
 				// Acquiring keyboard
 				mNotesEditText.requestFocus();
-				SoftKeyboard.showSoftKeyboard(HomeActivity.this, mNotesEditText);
+				SoftKeyboard
+						.showSoftKeyboard(HomeActivity.this, mNotesEditText);
 				// Changing notes button
 				mSlidingButton.setBackgroundResource(R.drawable.btn_closenotes);
 				// Starting task
 				cancelNotesTask();
 				mNotesTask = new NotesAsyncTask();
 				mNotesTask.execute(false);
-				Toast.makeText(HomeActivity.this, getString(R.string.notes_hint), Toast.LENGTH_LONG).show();
+				Toast.makeText(HomeActivity.this,
+						getString(R.string.notes_hint), Toast.LENGTH_LONG)
+						.show();
 			}
 		});
 		mSlidingDrawer.setOnDrawerCloseListener(new OnDrawerCloseListener() {
 			@Override
 			public void onDrawerClosed() {
 				// Hiding keyboard
-				SoftKeyboard.hideSoftKeyboard(HomeActivity.this, mNotesEditText);
+				SoftKeyboard
+						.hideSoftKeyboard(HomeActivity.this, mNotesEditText);
 				// Changing notes button
 				mSlidingButton.setBackgroundResource(R.drawable.btn_opennotes);
 				if (areNotesChanged() && isOwned()) {
@@ -203,8 +228,6 @@ public class HomeActivity extends SherlockFragmentActivity implements FragmentLo
 				}
 			}
 		});
-
-		initialized = true;
 	}
 
 	@Override
@@ -224,7 +247,8 @@ public class HomeActivity extends SherlockFragmentActivity implements FragmentLo
 		// Cancelling any active task
 		cancelNotesTask();
 		// Closing sliding drawer
-		mSlidingDrawer.close();
+		if(mSlidingDrawer!=null)
+			mSlidingDrawer.close();
 	}
 
 	private void cancelNotesTask() {
@@ -241,7 +265,8 @@ public class HomeActivity extends SherlockFragmentActivity implements FragmentLo
 		}
 		// Starting transaction
 		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-		Fragment fragment = (Fragment) Fragment.instantiate(this, frag.getName());
+		Fragment fragment = (Fragment) Fragment.instantiate(this,
+				frag.getName());
 		// Setting data
 		if (args != null) {
 			fragment.setArguments(args);
@@ -328,13 +353,15 @@ public class HomeActivity extends SherlockFragmentActivity implements FragmentLo
 
 		if (requestCode == SCAccessProvider.SC_AUTH_ACTIVITY_REQUEST_CODE) {
 			if (resultCode == RESULT_OK) {
-				String token = data.getExtras().getString(AccountManager.KEY_AUTHTOKEN);
+				String token = data.getExtras().getString(
+						AccountManager.KEY_AUTHTOKEN);
 				if (token == null) {
 					PMHelper.endAppFailure(this, R.string.app_failure_security);
 				} else {
 					initData(null);
 				}
-			} else if (resultCode == RESULT_CANCELED && requestCode == SCAccessProvider.SC_AUTH_ACTIVITY_REQUEST_CODE) {
+			} else if (resultCode == RESULT_CANCELED
+					&& requestCode == SCAccessProvider.SC_AUTH_ACTIVITY_REQUEST_CODE) {
 				PMHelper.endAppFailure(this, R.string.token_required);
 			}
 		}
@@ -348,58 +375,74 @@ public class HomeActivity extends SherlockFragmentActivity implements FragmentLo
 	private class NotesAsyncTask extends SCAsyncTask<Boolean, Void, String> {
 
 		public NotesAsyncTask() {
-			super(HomeActivity.this, new AbstractAsyncTaskProcessor<Boolean, String>(HomeActivity.this) {
-				@Override
-				public String performAction(Boolean... params) throws SecurityException, Exception {
-					if (params[0]) {
-						PMHelper.setNotes(mNotesEditText.getText().toString());
-					}
-					return PMHelper.getNotes();
-				}
+			super(HomeActivity.this,
+					new AbstractAsyncTaskProcessor<Boolean, String>(
+							HomeActivity.this) {
+						@Override
+						public String performAction(Boolean... params)
+								throws SecurityException, Exception {
+							if (params[0]) {
+								PMHelper.setNotes(mNotesEditText.getText()
+										.toString());
+							}
+							return PMHelper.getNotes();
+						}
 
-				@Override
-				public void handleResult(String result) {
-					mNotesEditText.setText(result);
-				}
-			});
+						@Override
+						public void handleResult(String result) {
+							mNotesEditText.setText(result);
+						}
+					});
 		}
 	}
 
 	/**
 	 * AsyncTask that allows to store and retrieve notes
 	 */
-	private class LoadPortfolioAsyncTask extends SCAsyncTask<String, Void, Portfolio> {
+	private class LoadPortfolioAsyncTask extends
+			SCAsyncTask<String, Void, Portfolio> {
 
 		public LoadPortfolioAsyncTask() {
-			super(HomeActivity.this, new AbstractAsyncTaskProcessor<String, Portfolio>(HomeActivity.this) {
-				@Override
-				public Portfolio performAction(String... params) throws SecurityException, Exception {
-					Portfolio p = null;
-					if (params != null && params.length > 0 && params[0] != null) {
-						owned = PMHelper.isOwnPortfolio(params[0]);
-						if (owned) {
-							p = PMHelper.findPortfolio(params[0]);
-						} else {
-							sharedPortfolioContainer = PMHelper.getSharedPortfolioContainer(params[0]);
-							if (sharedPortfolioContainer == null) return null;
-							p = sharedPortfolioContainer.getPortfolio();
+			super(HomeActivity.this,
+					new AbstractAsyncTaskProcessor<String, Portfolio>(
+							HomeActivity.this) {
+						@Override
+						public Portfolio performAction(String... params)
+								throws SecurityException, Exception {
+							Portfolio p = null;
+							if (params != null && params.length > 0
+									&& params[0] != null) {
+								owned = PMHelper.isOwnPortfolio(params[0]);
+								if (owned) {
+									p = PMHelper.findPortfolio(params[0]);
+								} else {
+									sharedPortfolioContainer = PMHelper
+											.getSharedPortfolioContainer(params[0]);
+									if (sharedPortfolioContainer == null)
+										return null;
+									p = sharedPortfolioContainer.getPortfolio();
+								}
+							}
+							return p;
 						}
-					}
-					return p;
-				}
 
-				@Override
-				public void handleResult(Portfolio result) {
-					if (result != null) {
-						FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-						Fragment frag = new PortfolioFragment();
-						frag.setArguments(PortfolioFragment.prepareArguments(result, false));
-						ft.replace(R.id.fragment_container, frag).commit();
-					} else {
-						Toast.makeText(HomeActivity.this, R.string.not_found_portfolios, Toast.LENGTH_LONG).show();
-					}
-				}
-			});
+						@Override
+						public void handleResult(Portfolio result) {
+							if (result != null) {
+								FragmentTransaction ft = getSupportFragmentManager()
+										.beginTransaction();
+								Fragment frag = new PortfolioFragment();
+								frag.setArguments(PortfolioFragment
+										.prepareArguments(result, false));
+								ft.replace(R.id.fragment_container, frag)
+										.commit();
+							} else {
+								Toast.makeText(HomeActivity.this,
+										R.string.not_found_portfolios,
+										Toast.LENGTH_LONG).show();
+							}
+						}
+					});
 		}
 	}
 
